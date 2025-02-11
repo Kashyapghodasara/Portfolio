e10305ac89b38250129632853b956a3e0258ca7f
Kashyap Ghodasara <kashyapghodasara007@gmail.com>
2025-02-11 17:42:17 +0530

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+   base: '/Portfolio',
   server: {
     port: process.env.PORT ? +process.env.PORT : 5173,
     host: '0.0.0.0',
