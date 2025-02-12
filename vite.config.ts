9637bee6b6e510e165ce527d52d5137b6f572f89
Kashyapghodasara <kashyapghodasara007@gmail.com>
2025-02-12 15:00:43 +0530

    Changess

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
-   base: '/Portfolio',
+  base: "./", // 🔹 Force relative paths
   server: {
     port: process.env.PORT ? +process.env.PORT : 5173,
     host: '0.0.0.0',
-  },  
+  },
   optimizeDeps: {
     exclude: ['lucide-react'], // Exclude unnecessary dependencies
   },
